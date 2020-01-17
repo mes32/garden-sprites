a9e7f09d8ba68c0e2cc6e575f3b28ecb32392664
mes32 <stockman.mike@gmail.com>
2020-01-17 12:43:47 -0600

    Improve CSS for nav-bar

--- src/garden_sprites/components/nav-bar.cljs
+++ src/garden_sprites/components/nav-bar.cljs
@@ -3,11 +3,12 @@
             [garden-sprites.routes :refer [refs]]))
 
 (defn nav-bar []
-  [:nav {:class "nav-bar"}
-    [:a {:href (rfe/href (:home refs))} [:h1 "Garden Sprites"]]
-    [:div
-      [:a {:href (rfe/href (:home refs))} "Home"]
-      [:a {:href (rfe/href (:epiphytes refs))} "Epiphytes"]
-      [:a {:href (rfe/href (:herbs refs))} "Herbs"]
-      [:a {:href (rfe/href (:succulents refs))} "Succulents"]
-      [:a {:href (rfe/href (:shopping-cart refs))} "Cart (0)"]]])
+  [:nav {:id "nav-bar"}
+    [:div {:class "content-div"}
+      [:a {:href (rfe/href (:home refs))} [:h1 "Garden Sprites"]]
+      [:div
+        [:a {:href (rfe/href (:home refs))} "Home"]
+        [:a {:href (rfe/href (:epiphytes refs))} "Epiphytes"]
+        [:a {:href (rfe/href (:herbs refs))} "Herbs"]
+        [:a {:href (rfe/href (:succulents refs))} "Succulents"]
+        [:a {:href (rfe/href (:shopping-cart refs))} "Cart (0)"]]]])
