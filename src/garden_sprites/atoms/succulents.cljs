ed6530c9c2cccc4fc7bc0e701093a68ca3b7dca8
mes32 <stockman.mike@gmail.com>
2020-01-18 18:18:22 -0600

    Add an atom with a list of succulents

---
+++ src/garden_sprites/atoms/succulents.cljs
@@ -0,0 +1,26 @@
+; Photo by Maureen Bekker from Pexels
+; Echeveria 'Fleur Blanc'
+; This plant grows as a rosette of succulent lobes. As native plants growing in semi-arid areas, they are robust and easy to maintain.
+
+; https://www.pexels.com/photo/shallow-focus-photo-of-aloe-vera-plants-3234638/
+; Photo by Himesh Mehta from Pexels
+; Spotted Aloe Vera
+
+; https://www.pexels.com/photo/botanical-cactus-cactus-garden-cactus-plant-291544/
+; Photo by Abhinav Goswami from Pexels
+; Cactus
+
+(ns garden-sprites.atoms.succulents
+  (:require [reagent.core :as r]))
+
+(defonce succulents (r/atom
+  [
+    {:name "Echeveria 'Fleur Blanc'"
+     :img "./images/green-echeveria.jpg"
+     :price 20.00}
+    {:name "Spotted Aloe Vera"
+     :img "./images/spotted-aloe-vera.jpg"
+     :price 15.50}
+    {:name "Cactus"
+     :img "./images/cactus.jpg"
+     :price 10.20}]))
